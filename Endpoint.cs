536ee46919a71134c3b0ddb6ff26c68c36cf7560
DESKTOP-LQB14OB\karol <karol.gongola@gmail.com>
2018-04-08 20:14:17 +0200

    update structure

--- Endpoint.cs
+++ Endpoint.cs
@@ -5,8 +5,16 @@ namespace genetic
 {
     public class Endpoint
     {
+        public int id;
         public int latency_to_server;//ms
         public int number_of_cache_servers;
         public List<Connection> connections_to_servers = new List<Connection>();
+
+        public Endpoint(int id, int latency_to_server, int number_of_cache_servers)
+        {
+            this.id = id;
+            this.latency_to_server = latency_to_server;
+            this.number_of_cache_servers = number_of_cache_servers;
+        }
     }
 }
--- InputReader.cs
+++ InputReader.cs
@@ -20,14 +20,33 @@ namespace genetic
             data_model.number_of_cache_servers_C = Int32.Parse(first_line[3]);
             data_model.capacity_of_server_X = Int32.Parse(first_line[4]);
 
+            for(int i = 0; i < data_model.number_of_cache_servers_C; i++)
+            {
+                data_model.servers.Add(new Server(i, data_model.capacity_of_server_X));
+            }
+
             var video_sizes = file_lines[1].Split(' ');
             for(int i = 0; i < video_sizes.Count(); i++)
             {
                 data_model.videos.Add(new Video(i, Int32.Parse(video_sizes[i])));
             }
 
+            var endpoint_lines = file_lines.ToList();
+            endpoint_lines.RemoveAt(0);
+            endpoint_lines.RemoveAt(0);
 
-            Console.WriteLine(file_lines[0]);
+            for(int i = 0; i < data_model.number_of_endpoints_E; i++)
+            {
+                var endpoint_line = endpoint_lines[i].Split(' ');
+                Endpoint endpoint = new Endpoint(i, Int32.Parse(endpoint_line[0]), Int32.Parse(endpoint_line[1]));
+                endpoint_lines.RemoveAt(0);
+                for(int j = 0; j < endpoint.number_of_cache_servers; j++)
+                {
+                    
+                }
+            }
+
+            Console.WriteLine(endpoint_lines[0]);
             return data_model;
         }
     }
