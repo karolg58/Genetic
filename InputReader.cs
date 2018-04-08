5594bb36fa31e3a37e5686d75ae3d16be6bdb15f
DESKTOP-LQB14OB\karol <karol.gongola@gmail.com>
2018-04-08 19:57:04 +0200

    update

--- InputReader.cs
+++ InputReader.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace genetic
 {
@@ -18,8 +19,16 @@ namespace genetic
             data_model.number_of_requests_R = Int32.Parse(first_line[2]);
             data_model.number_of_cache_servers_C = Int32.Parse(first_line[3]);
             data_model.capacity_of_server_X = Int32.Parse(first_line[4]);
+
+            var video_sizes = file_lines[1].Split(' ');
+            for(int i = 0; i < video_sizes.Count(); i++)
+            {
+                data_model.videos.Add(new Video(i, Int32.Parse(video_sizes[i])));
+            }
+
+
             Console.WriteLine(file_lines[0]);
-            return null;
+            return data_model;
         }
     }
 }
