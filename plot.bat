69a3f47ff05820399d466e3280a05800ac24c87c
DESKTOP-LQB14OB\karol <karol.gongola@gmail.com>
2018-04-22 21:44:01 +0200

    update

--- plot.bat
+++ plot.bat
@@ -1,7 +1,7 @@
-python plot.py -v 1_kittens_default 1_kittens_emas
-
 python plot.py -v 1_me_at_the_zoo_default 1_me_at_the_zoo_emas
+
+python plot.py -v 1_videos_worth_spreading_default 1_videos_worth_spreading_emas
 
 python plot.py -v 1_trending_today_default 1_trending_today_emas
 
-python plot.py -v 1_videos_worth_spreading_default 1_videos_worth_spreading_emas
+python plot.py -v 1_kittens_default 1_kittens_emas
